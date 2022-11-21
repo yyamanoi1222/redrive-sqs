8ade033b87e2240f66cc0d975eb55bacb58e2f65
Yuu yamanoi <yuu.yamanoi1222@gmail.com>
2022-11-21 22:49:06 +0900

    use flag

--- cmd/redrive-sqs/main.go
+++ cmd/redrive-sqs/main.go
@@ -1,53 +1,29 @@
 package main
 
 import (
-  "github.com/urfave/cli/v2"
   "github.com/yyamanoi1222/redrive-sqs/internal/runner"
+  "flag"
   "os"
   "fmt"
 )
 
 func main() {
-  cli.AppHelpTemplate = `NAME:
-   {{.Name}}
-USAGE:
-   {{.HelpName}} {{if .Commands}} command [command options]{{end}}
-   {{if len .Authors}}
-AUTHOR:
-   {{range .Authors}}{{ . }}{{end}}
-   {{end}}{{if .Commands}}
-COMMANDS:
-{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .Copyright }}
-COPYRIGHT:
-   {{.Copyright}}
-   {{end}}{{if .Version}}
-VERSION:
-   {{.Version}}
-   {{end}}
-`
-  app := cli.App{
-    Name: "redrive-sqs",
-    Version: "v0.0.0",
-    Action: func(cCtx *cli.Context) error {
-      return runner.Redrive(
-        cCtx.String("src"),
-        cCtx.String("dest"),
-      )
-    },
-    Flags: []cli.Flag{
-      &cli.StringFlag{
-        Name: "src",
-        Usage: "Source Queue URL",
-        Required: true,
-      },
-      &cli.StringFlag{
-        Name: "dest",
-        Usage: "Destination Queue URL",
-        Required: true,
-      },
-    },
+  var (
+    src string
+    dest string
+  )
+
+  flag.StringVar(&src, "src", "", "src dead letter queue url")
+  flag.StringVar(&dest, "dest", "", "destination queue url")
+  flag.Parse()
+
+
+  if len(src) == 0 || len(dest) == 0 {
+    fmt.Fprintf(os.Stderr, "invalid arguments \n")
+    os.Exit(1)
   }
-  if err := app.Run(os.Args); err != nil {
-    fmt.Printf("Error %s", err)
+
+  if err := runner.Redrive(src, dest); err != nil {
+    fmt.Fprintf(os.Stderr, "%s \n", err)
   }
 }
--- go.mod
+++ go.mod
@@ -2,14 +2,6 @@ module github.com/yyamanoi1222/redrive-sqs
 
 go 1.17
 
-require (
-	github.com/aws/aws-sdk-go v1.44.91
-	github.com/urfave/cli/v2 v2.23.5
-)
+require github.com/aws/aws-sdk-go v1.44.91
 
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-)
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
